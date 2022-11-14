76c287127768d5c81fbf836c59aacaf0447d4a9b
Jehan Punzalan <jcpunzalan@bayad.com>
2022-11-14 22:08:01 +0800

    added authentication feature and fix routes

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,14 +1,34 @@
+import React, { useEffect } from "react";
 import SearchBar from "../components/searchBar";
+import { useAuth0 } from "@auth0/auth0-react";
+
+import NoDP from "../assets/images/no-dp.jpg";
+
 export function HomePage() {
+  const { user, isAuthenticated } = useAuth0();
+
   return (
-    <div className="home-container">
+    isAuthenticated && (
+      <div className="home-container">
         <div className="home-header">
-            <label htmlFor="">John Smith</label>
-            <p>https://github.com/smithjohn</p>
+          <div className="home-avatar">
+            {user.picture !== "" ? (
+              <img src={user.picture} alt="avatar" className="home-avatar" />
+            ) : (
+              <img src={NoDP} className="home-avatar" />
+            )}
+          </div>
+          <div className="home-user-info">
+            <h2>{user.name === "" ? user.nickname : user.name}</h2>
+            <a href={`https://github.com/${user.nickname}`} target="_blank">
+              https://github.com/{user.nickname}
+            </a>
+          </div>
         </div>
         <div className="home-body">
-            <SearchBar placeHolder="Enter City" />
+          <SearchBar placeHolder="Enter City" />
         </div>
-    </div>
+      </div>
+    )
   );
 }
--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,3 +1,12 @@
+import Login from "../components/login";
 export function LandingPage() {
-    return <h1>LandingPage</h1>
+  return (
+    <div className="landing-page-container">
+      <p>
+        Welcome to the weather forecast web application. Please login with your
+        Github user to use the application and view the weather in your city.
+      </p>
+      <Login />
+    </div>
+  );
 }
